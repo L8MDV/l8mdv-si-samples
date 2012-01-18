b14544a8e40c7c1a47c6e223baec1d252c8433bc
Matt <matt.d.vickery@gmail.com>
2012-01-17 18:00:24 -0800

    General new file update.

---
+++ splitter-aggregator/src/main/java/com/l8mdv/sample/service/BrokerQuoteRequestSplitter.java
@@ -0,0 +1,12 @@
+package com.l8mdv.sample.service;
+
+/**
+ * Copyright Matt Vickery 2012
+ *
+ * @author matt.d.vickery@gmail.com
+ * @since 17/01/2012
+ */
+public class BrokerQuoteRequestSplitter {
+
+
+}
