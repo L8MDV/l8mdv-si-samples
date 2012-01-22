6b630115d4e83a8eb1c0a7c06777a62ae70cd486
Matt <matt.d.vickery@gmail.com>
2012-01-22 07:40:33 -0800

    New modules.

--- header-management/src/test/java/com/l8mdv/sample/ClaimCheckIntegrationTest.java
+++ header-management/src/test/java/com/l8mdv/sample/ClaimCheckIntegrationTest.java
@@ -11,12 +11,6 @@ import org.springframework.test.context.junit4.SpringJUnit4ClassRunner;
 
 import static com.l8mdv.sample.ClaimCheckGateway.CLAIM_CHECK_ID;
 
-/**
- * Copyright Matt Vickery 2012
- *
- * @author matt.d.vickery@gmail.com
- * @since 21/01/2012
- */
 @RunWith(SpringJUnit4ClassRunner.class)
 @ContextConfiguration(
     locations = {"classpath:META-INF/spring/claim-check.xml"}
@@ -28,10 +22,14 @@ public class ClaimCheckIntegrationTest {
     @Test
     public void locatePayloadInHeader() {
         String payload = "Sample test message.";
-        Message<String> message = MessageBuilder.withPayload(payload).build();
-        Message<String> response = claimCheckGateway.send(message);
+        Message<String> message =
+                MessageBuilder.withPayload(payload).build();
+        Message<String> response =
+                claimCheckGateway.send(message);
 
-        Assert.assertTrue(response.getPayload().equals(payload));
-        Assert.assertTrue(response.getHeaders().get(CLAIM_CHECK_ID) != null);
+        Assert.assertTrue(response.getPayload()
+                .equals(payload));
+        Assert.assertTrue(response.getHeaders()
+                .get(CLAIM_CHECK_ID) != null);
     }
 }
